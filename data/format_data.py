45b7782bd732545ed82a4e5c6f1e166e62426c4d
suzanneguerrier25 <suzanne.guerrier25@trinityschoolnyc.org>
2024-04-03 23:33:34 -0400

    fixed issue with domestic, birds being split

--- data/format_data.py
+++ data/format_data.py
@@ -6,10 +6,13 @@ lines = f.readlines()
 
 dictionary = {}
 
+# EDIT : went through the csv and removed all Domestic,Birds and changed them to Domestic Birds to facilitate strip() (with ctrlf)
+
 # iterates through all the "rows" in the data
 for line in lines: 
     line = line.split(",")  #splits the line into different sections of data (ex. number, location) by comma
-    line[6].strip() #removes newline from the data
+    for attr in line:
+        attr = attr.strip()
     dictionary[line[0]] = [line[1],line[2],line[3],line[4],line[5],line[6]] # adds to dictionary with the date and time as a key (to have separate data, only column with no overlap)
 
 f.close()
