a1e72a7014b83a061ab802d849a9060934cb9780
suzanneguerrier25 <suzanne.guerrier25@trinityschoolnyc.org>
2024-04-04 16:08:20 -0400

    edited data formatting in formatdata.py

--- data/format_data.py
+++ data/format_data.py
@@ -11,14 +11,14 @@ dictionary = {}
 # iterates through all the "rows" in the data
 for line in lines: 
     
-    line = line.split(",")
-    for attr in line:
-        attr= attr.strip()
-        
-    dictionary[line[0]] = [line[1],line[2],line[3],line[4],line[5],line[6]] # adds to dictionary with the date and time as a key (to have separate data, only column with no overlap)
-    #print(dictionary)
+    line = line.split(",") #splits the line into different sections of data (ex. number, location) by comma
 
-    #line = line.split(",")  #splits the line into different sections of data (ex. number, location) by comma
+    # removes unnecessary formatting 
+    line[6] = line[6].replace('\n',"")
+
+    
+    dictionary[line[0]] = [line[1],line[2],line[3],line[4],line[5],line[6]] # adds to dictionary with the date and time as a key (to have separate data, only column with no overlap)
+
     
 f.close()
 
