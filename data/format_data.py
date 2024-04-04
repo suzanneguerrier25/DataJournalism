b66b90f469baebbdb2ee5fe85a92a1bcf63842e4
suzanneguerrier25 <suzanne.guerrier25@trinityschoolnyc.org>
2024-04-03 20:58:23 -0400

    formatted the data in format_data, added to data.json

--- data/format_data.py
+++ data/format_data.py
@@ -1,5 +1,22 @@
 import json
 
-f = open("cleanddata.csv","r")
+f = open("data/cleaneddata.csv","r")
+
 lines = f.readlines()
 
+dictionary = {}
+
+# iterates through all the "rows" in the data
+for line in lines: 
+    line = line.split(",")  #splits the line into different sections of data (ex. number, location) by comma
+    line[6].strip() #removes newline from the data
+    dictionary[line[0]] = [line[1],line[2],line[3],line[4],line[5],line[6]] # adds to dictionary with the date and time as a key (to have separate data, only column with no overlap)
+
+f.close()
+
+f1= open("data/data.json","w")
+
+
+json.dump(dictionary,f1,indent=4) #adds the data to a json file
+
+f1.close()
