3d3313e887e63181ea5dd161cbb777bc5c653927
suzanneguerrier25 <suzanne.guerrier25@trinityschoolnyc.org>
2024-04-15 10:41:34 -0400

    started code for hosting the flask server

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='static')
+
+app.run(debug=True)
+
+@app.route('/')
+def about():
+    f = open("data/data.json","r")
