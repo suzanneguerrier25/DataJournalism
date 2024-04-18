37e96be74726342be5f2e00b48a1caeb1e62bfca
suzanneguerrier25 <suzanne.guerrier25@trinityschoolnyc.org>
2024-04-18 15:59:58 -0400

    nav bar, routing

--- server.py
+++ server.py
@@ -1,12 +1,28 @@
 from flask import Flask
+from flask import render_template
+from flask import request
+
 
 app = Flask(__name__,
             static_url_path='',
             static_folder='static')
 
-app.run(debug=True)
+
 
 @app.route('/')
-def about():
+def macro():
     f = open("data/data.json","r")
-    
+    return render_template("macro.html")
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/micro')
+def micro():
+    borough = request.args.get('/borough')
+    print(borough,"test")
+    return render_template('micro.html')
+   
+app.run(debug=True)
