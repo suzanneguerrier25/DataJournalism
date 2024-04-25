c4e23abea1089caf8ed8e425a78ca1993f1bb9de
suzanneguerrier25 <suzanne.guerrier25@trinityschoolnyc.org>
2024-04-25 10:58:58 -0400

    edited svg file w opacities

--- server.py
+++ server.py
@@ -14,7 +14,27 @@ app = Flask(__name__,
 def macro():
     f = open("data/data.json","r")
     data=json.load(f)
-    return render_template("macro.html",boroughs= data.keys())
+
+    brkbirdnum=0
+    brnxbirdnum=0
+    qubirdnum=0
+    manbirdnum=0
+    sibirdnum=0
+    for borough in data:
+        for call in data[borough]:
+            if borough== "Manhattan":
+                manbirdnum+=(data[borough][call][4])/200
+            elif borough== "Bronx":
+                brnxbirdnum+=(data[borough][call][4])/200
+            elif borough=="Queens":
+                qubirdnum+=(data[borough][call][4])/200
+            elif borough == "Staten Island":
+                sibirdnum+=(data[borough][call][4])/200
+            elif borough=="Brooklyn":
+                brkbirdnum+=(data[borough][call][4])/200
+    print(brkbirdnum,manbirdnum,qubirdnum,sibirdnum,brnxbirdnum)
+
+    return render_template("macro.html",boroughs= data.keys(),brkbirdnum=brkbirdnum,manbirdnum=manbirdnum,qubirdnum=qubirdnum,sibirdnum=sibirdnum,brnxbirdnum=brnxbirdnum)
 
 @app.route('/about')
 def about():
